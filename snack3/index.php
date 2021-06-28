170cffc5a5738ef8847f2ca8a6586cbb4ff6e8f9
Lale98 <78683721+Lale98@users.noreply.github.com>
2021-06-28 16:03:23 +0200

    snack3

--- snack3/index.php
+++ snack3/index.php
@@ -1,2 +1,20 @@
 ## Snack 3
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+
+<?php 
+
+    $numbers = [];
+    
+    do {
+        $num = rand(1, 100);
+        
+        if (!in_array($num, $numbers)) {
+            $numbers[] = $num;
+        }
+    } while (count($numbers) < 15);
+    
+    echo "<pre>";
+    print_r($numbers);
+    echo "</pre>";
+
+?>
